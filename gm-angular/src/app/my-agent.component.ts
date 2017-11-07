6bf0ca3519b27e08ae60f592fbf74b53b0284988
zhouli121018 <17607619682@163.com>
2017-11-07 17:17:40 +0800

    重写搜索表单

--- gm-angular/src/app/my-agent.component.ts
+++ gm-angular/src/app/my-agent.component.ts
@@ -7,58 +7,7 @@ import {AgentService} from './agent.service';
 @Component({
 	selector:'my-agent',
 	styleUrls:['./my-agent.component.css'],
-	template:`
-	<h2>
-	    <span>{{title}}</span>
-		<img src="./assets/img/logo.png"/>
-	</h2>
-	<div class=''>
-    <div class="searchInfo">
-          <div class="row">
-        <div class="col-sm-3">
-          <div class="input-group">
-            <span class="input-group-btn">
-              <button class="btn btn-default" type="button">起始时间</button>
-            </span>
-            <input type="date" class="form-control" placeholder="选择日期">
-          </div>
-        </div>
-        <div class="col-sm-3">
-          <div class="input-group">
-            <span class="input-group-btn">
-              <button class="btn btn-default" type="button">结束时间</button>
-            </span>
-            <input type="date" class="form-control" placeholder="选择日期">
-          </div>
-        </div>
-            <div class="col-sm-3">
-            <div class="input-group">
-              <span class="input-group-btn">
-                <button class="btn btn-default" type="button">代理邀请码</button>
-              </span>
-              <input type="number" class="form-control" placeholder="代理邀请码">
-            </div>
-        </div>
-        <div class="col-sm-3">
-              <button class="btn btn-success" type="button">查询</button>
-        </div>
-      </div>
-    </div>
-		<table class="table table-bordered table-responsive table-striped">
-			<tr>
-				<th>代理编号</th>
-				<th>代理姓名</th>
-				<th>代理微信</th>
-			</tr>
-			<tr *ngFor="let agent of agents">
-				<td>{{agent.id}}</td>
-				<td>{{agent.name}}</td>
-				<td>{{agent.weixin}}</td>
-			</tr>
-		</table>
-	</div>
-  <app-echarts ></app-echarts>
-	`
+	templateUrl:'./my-agent.component.html'
 })
 export class MyAgentComponent implements OnInit{
 	title = "我的代理";
--- gm-angular/src/app/my-vip.component.ts
+++ gm-angular/src/app/my-vip.component.ts
@@ -5,65 +5,7 @@ import { OnInit } from '@angular/core';
 @Component({
 	selector:'my-vip',
 	styles:[],
-	template:`
-      <h2>
-      	<span>{{title}}</span>
-		<img src="./assets/img/logo.png"/>
-      </h2>
-      <div>
-      <div class="searchInfo">
-	      	<div class="row">
-			  <div class="col-sm-3">
-			    <div class="input-group">
-			      <span class="input-group-btn">
-			        <button class="btn btn-default" type="button">起始时间</button>
-			      </span>
-			      <input type="date" class="form-control" placeholder="选择日期">
-			    </div>
-			  </div>
-			  <div class="col-sm-3">
-			    <div class="input-group">
-			      <span class="input-group-btn">
-			        <button class="btn btn-default" type="button">结束时间</button>
-			      </span>
-			      <input type="date" class="form-control" placeholder="选择日期">
-			    </div>
-			  </div>
-		      	<div class="col-sm-3">
-				    <div class="input-group">
-				      <span class="input-group-btn">
-				        <button class="btn btn-default" type="button">用 户 ID</button>
-				      </span>
-				      <input type="number" class="form-control" placeholder="用户ID">
-				    </div>
-				</div>
-			  <div class="col-sm-3">
-			        <button class="btn btn-success" type="button">查询</button>
-			  </div>
-			</div>
-		</div>
-      	<table class="table table-bordered table-responsive table-striped">
-			<tr>
-				<th>用户ID</th>
-				<th>用户昵称</th>
-				<th>剩余红钻</th>
-				<th>剩余房卡</th>
-				<th>创建时间</th>
-			</tr>
-			<tbody *ngIf="vips">
-				<tr *ngFor="let vip of vips">
-					<td>{{vip.uuid}}</td>
-					<td>{{vip.nickname}}</td>
-					<td>{{vip.redcard}}</td>
-					<td>{{vip.roomcard}}</td>
-					<td>{{vip.createtime}}</td>
-				</tr>
-			</tbody>
-		</table>
-      </div>
-      <app-paging [pageIndex]="this.pageIndex" [pageCount]="this.pageCount" (change)="bindpage($event)"></app-paging>
-      <p class="total">共有 <b>{{totalNum}}</b> 条记录</p>
-	`
+	templateUrl:'./my-vip.component.html'
 	})
 export class MyVipComponent implements OnInit{
 	title = "我的会员";
